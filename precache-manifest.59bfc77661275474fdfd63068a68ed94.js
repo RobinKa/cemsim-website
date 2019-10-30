4b642916c22df468696f390555476040fe06e51c
Robin Kahlow <robinkahlow@web.de>
2019-10-30 13:26:56 +0000

    initial

---
+++ precache-manifest.59bfc77661275474fdfd63068a68ed94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb4a7def206306d70386c88d0521e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "498e15af5b37570e2427",
+    "url": "/static/css/main.abc54a37.chunk.css"
+  },
+  {
+    "revision": "8ff70cf9095149d24d5a",
+    "url": "/static/js/2.2807b47d.chunk.js"
+  },
+  {
+    "revision": "498e15af5b37570e2427",
+    "url": "/static/js/main.bff1c08b.chunk.js"
+  },
+  {
+    "revision": "fe8742e903f233224cb0",
+    "url": "/static/js/runtime-main.1aa9e89d.js"
+  }
+]);
